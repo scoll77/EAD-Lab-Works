a5ec71a26d33eac861a8a48e69d0cd8f22feaef2
scoll77 <june.00447@gmail.com>
2025-01-03 13:52:47 +0300

    Lab Exam

---
+++ LabExam/k/src/test/java/com/naolk/k/KApplicationTests.java
@@ -0,0 +1,13 @@
+package com.naolk.k;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
